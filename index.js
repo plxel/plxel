549064d589a7b5f03bd44ed1e4b7ab45914a33d2
Alexey Mikhaylov <alexeym.vn@gmail.com>
2021-01-15 10:10:07 +0300

    add more info

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 const welcome = require('cli-welcome');
+const chalk = require('chalk')
+const sym = require('log-symbols')
 const packageJson = require('./package.json')
+
+const log = console.log;
+
+const socials = [
+  { name: 'LinkedIn', color: '#0077b5', link: 'https://www.linkedin.com/in/mikhaylovalexey/' },
+  { name: 'Github', color: '#6e5494', link: 'https://github.com/plxel' },
+  { name: 'Blog', color: '#fafafa', link: 'https://todo.dev' },
+  { name: 'Telegram', color: '#0088cc', link: 'https://t.me/alexey_mikhaylov' },
+]
+
+const maxNameLength = Math.max(...socials.map(({ name }) => name.length))
+const bgTitle = (color, title) => chalk.bgHex(color)(` ${title} `)
+
+const makeSocialLine = ({ name, link, color }) => 
+  `${bgTitle(color, name)}${name.padEnd(maxNameLength, ' ').replace(name, '')} ${chalk.dim(link)}`
 
 welcome({
 	title: packageJson.name,
@@ -11,3 +28,18 @@ welcome({
 	clear: true,
 	version: packageJson.version,
 });
+
+
+log(`Hey, I’am software developer, love working remotely
+
+Stack which I happy with:
+${sym.success} ${chalk.hex('#00d8ff')('React')}
+${sym.success} ${chalk.hex('#215732')('Node')}
+${sym.success} ${chalk.hex('#e00097')('GraphQL')}
+${sym.success} ${chalk.hex('#3178c6')('TypeScript')}*
+
+${chalk.underline('_'.repeat(80))}
+
+How to reach me:
+${socials.map(makeSocialLine).join`\n`}
+`)
